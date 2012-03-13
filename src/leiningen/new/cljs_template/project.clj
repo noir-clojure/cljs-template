6e3e7e34d3c2d0aaa02e5fd50f43612b7b1dc568
Chris Granger <ibdknox@gmail.com>
2012-03-13 12:28:38 -0700

    inc version
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- src/leiningen/new/cljs_template/project.clj
+++ src/leiningen/new/cljs_template/project.clj
@@ -2,10 +2,10 @@
   :description "FIXME: write this!"
   :url "http://exampl.com/FIXME"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [noir-cljs "0.2.3"]
+                 [noir-cljs "0.2.4"]
                  [jayq "0.1.0-alpha1"]
                  [fetch "0.1.0-alpha2"]
-                 [crate "0.1.0-alpha1"]
+                 [crate "0.1.0-alpha2"]
                  [noir "1.3.0-alpha10"]]
   :cljsbuild {:builds [{}]}
   :main ^{:skip-aot true} {{name}}.server)
