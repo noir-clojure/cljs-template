5c1f7eb3b2686107020c60f4a2e0bcb3f4582f10
Chris Granger <ibdknox@gmail.com>
2012-04-24 21:14:08 -0700

    follow the correct naming conventions for lein 2
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-template "0.1.5"
+(defproject cljs-template/lein-template "0.1.5"
   :description "A lein-newnew template for clojurescript projects"
   :url "http://github.com/ibdknox/cljs-template"
   :eval-in-leiningen true
